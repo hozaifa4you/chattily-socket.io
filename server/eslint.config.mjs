b43f5152c5c0b56bac2b2afde40c509ad689b78d
Aionline <aionline@Akhlasurs-MacBook-Pro.local>
2025-06-22 10:46:58 +0600

    Inital server setup

---
+++ server/eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+   {
+      files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+      plugins: { js },
+      extends: ['js/recommended'],
+   },
+   {
+      files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+      languageOptions: { globals: globals.browser },
+      rules: {
+         'no-console': ['warn', { allow: ['warn', 'error'] }],
+         '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' },
+         ],
+      },
+   },
+   tseslint.configs.recommended,
+   eslintConfigPrettier,
+]);
