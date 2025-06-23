fea095031e94088c55da2dd1210dba434ab65fdd
Hozaifa <hozaifabinmasud@gmail.com>
2025-06-23 20:49:51 +0600

    Chat migration

--- server/src/routes/profile.route.ts
+++ server/src/routes/profile.route.ts
@@ -1,7 +1,18 @@
-import express, { type Router } from 'express';
+import { getProfile, updateProfile } from '@/controllers/profile.controller';
+import { auth } from '@/middleware/auth.middleware';
+import express, { RequestHandler, type Router } from 'express';
 
 const profileRoutes: Router = express.Router();
 
-profileRoutes.get('/');
+profileRoutes
+   .route('/')
+   .get(
+      auth as unknown as RequestHandler,
+      getProfile as unknown as RequestHandler,
+   )
+   .put(
+      auth as unknown as RequestHandler,
+      updateProfile as unknown as RequestHandler,
+   );
 
 export { profileRoutes };
